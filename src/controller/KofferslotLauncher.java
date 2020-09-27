4007bd4131824366b16975aef3c0981d732ac7cf
StefanvanTilburg <TillyAliasStefan@gmail.com>
2020-09-27 16:52:16 +0200

    Opdracht 2.2 Kofferslot resultaat B --fix redundancy, Magic Nr

--- src/controller/KofferslotLauncher.java
+++ src/controller/KofferslotLauncher.java
@@ -5,60 +5,31 @@ import model.Kofferslot;
 /**
  * @author Stefan van Tilburg
  * <p>
- * Opdracht
+ * Opdracht 2.2 Kofferslot
  * <p>
- * Doel
+ * Doel     Maak een Kofferslot class, waarbij het cijferslot bestaat uit 2 letters [A-Z] en 1 cijfer [0-9]
  */
 public class KofferslotLauncher {
     public static void main(String[] args) {
-        // TODO print methode toevoegen aan Kofferslot, oogt netter dan al deze print statements
+        Kofferslot[] koffersloten = new Kofferslot[6];
+        koffersloten[0] = new Kofferslot();
+        koffersloten[1] = new Kofferslot('C','D',8);
+        koffersloten[2] = new Kofferslot('A', 'A', 0);
+        koffersloten[3] = new Kofferslot('B', 'R', 9);
+        koffersloten[4] = new Kofferslot('D', 'Z', 9);
+        koffersloten[5] = new Kofferslot('Z', 'Z', 9);
 
-        Kofferslot eersteKofferslot = new Kofferslot();
-        System.out.printf("%c%c%d\n", eersteKofferslot.getEersteLetter(),
-                eersteKofferslot.getTweedeLetter(), eersteKofferslot.getCijfer());
+        for (Kofferslot kofferslot : koffersloten) {
+            System.out.println(kofferslot);
+            kofferslot.volgende();
+            System.out.println(kofferslot);
+        }
 
-        Kofferslot tweedeKofferslot = new Kofferslot('C','D',8);
-        System.out.printf("%c%c%d\n", tweedeKofferslot.getEersteLetter(),
-                tweedeKofferslot.getTweedeLetter(), tweedeKofferslot.getCijfer());
-
-        tweedeKofferslot.setCombinatie('A', 'A', 0);
-        System.out.printf("%c%c%d\n", tweedeKofferslot.getEersteLetter(),
-                tweedeKofferslot.getTweedeLetter(), tweedeKofferslot.getCijfer());
-        tweedeKofferslot.volgende();
-        System.out.printf("%c%c%d\n", tweedeKofferslot.getEersteLetter(),
-                tweedeKofferslot.getTweedeLetter(), tweedeKofferslot.getCijfer());
-
-        tweedeKofferslot.setCombinatie('B', 'R', 9);
-        System.out.printf("%c%c%d\n", tweedeKofferslot.getEersteLetter(),
-                tweedeKofferslot.getTweedeLetter(), tweedeKofferslot.getCijfer());
-        tweedeKofferslot.volgende();
-        System.out.printf("%c%c%d\n", tweedeKofferslot.getEersteLetter(),
-                tweedeKofferslot.getTweedeLetter(), tweedeKofferslot.getCijfer());
-
-        tweedeKofferslot.setCombinatie('D', 'Z', 9);
-        System.out.printf("%c%c%d\n", tweedeKofferslot.getEersteLetter(),
-                tweedeKofferslot.getTweedeLetter(), tweedeKofferslot.getCijfer());
-        tweedeKofferslot.volgende();
-        System.out.printf("%c%c%d\n", tweedeKofferslot.getEersteLetter(),
-                tweedeKofferslot.getTweedeLetter(), tweedeKofferslot.getCijfer());
-
-        tweedeKofferslot.setCombinatie('Z', 'Z', 9);
-        System.out.printf("%c%c%d\n", tweedeKofferslot.getEersteLetter(),
-                tweedeKofferslot.getTweedeLetter(), tweedeKofferslot.getCijfer());
-        System.out.printf("Volgende cijfer is : %d\n", tweedeKofferslot.volgendeCijfer(tweedeKofferslot.getCijfer()));
-        tweedeKofferslot.volgende();
-        System.out.printf("%c%c%d\n", tweedeKofferslot.getEersteLetter(),
-                tweedeKofferslot.getTweedeLetter(), tweedeKofferslot.getCijfer());
-
-
-        tweedeKofferslot.setCombinatie('A', 'A', 0);
-        System.out.println("Fucking modulus");
-        for (int i = 0; i < 300; i++) {
-            //System.out.printf("Cijfer is : %d volgende cijfer is : %d\n",
-            //        tweedeKofferslot.getCijfer(), tweedeKofferslot.volgendeCijfer(tweedeKofferslot.getCijfer()));
-            System.out.printf("%c%c%d\n", tweedeKofferslot.getEersteLetter(),
-                    tweedeKofferslot.getTweedeLetter(), tweedeKofferslot.getCijfer());
-            tweedeKofferslot.volgende();
+        koffersloten[0].setCombinatie('A', 'A', 0);
+        System.out.println("Test slot modulus logica:");
+        for (int i = 0; i < 7000; i++) {
+            System.out.println(koffersloten[0]);
+            koffersloten[0].volgende();
         }
     }
 }
--- src/model/Kofferslot.java
+++ src/model/Kofferslot.java
@@ -3,12 +3,16 @@ package model;
 /**
  * @author Stefan van Tilburg
  * <p>
- * Opdracht
+ * Opdracht 2.2 Kofferslot
  * <p>
- * Doel
+ * Doel     Kofferslot class, waarbij het cijferslot bestaat uit 2 letters [A-Z] en 1 cijfer [0-9]
  */
 public class Kofferslot {
-    // TODO maak final variabelen aan: magic number party over here!
+    private static final char START_LETTER = 'A';
+    private static final char EIND_LETTER = 'Z';
+    private static final int START_CIJFER = 0;
+    private static final int EIND_CIJFER = 9;
+    private static final int VOLGENDE_ADDITIE = 1;
     private char eersteLetter;
     private char tweedeLetter;
     private int cijfer;
@@ -20,7 +24,7 @@ public class Kofferslot {
     }
 
     public Kofferslot() {
-        this('A', 'A', 0);
+        this(START_LETTER, START_LETTER, START_CIJFER);
     }
 
     public void setCombinatie(char eersteLetter, char tweedeLetter, int cijfer) {
@@ -30,21 +34,27 @@ public class Kofferslot {
     }
 
     public char volgendeLetter(char letter) {
-        return (char) ((letter - 'A' + 1) % ('Z' - 'A' + 1) + 'A');
+        return (char) ((letter - START_LETTER + VOLGENDE_ADDITIE) %
+                        (EIND_LETTER - START_LETTER + VOLGENDE_ADDITIE) + START_LETTER);
     }
 
     public int volgendeCijfer(int cijfer) {
-        return (cijfer + 1) % 10;
+        return (cijfer + VOLGENDE_ADDITIE) % (EIND_CIJFER + VOLGENDE_ADDITIE);
     }
 
     public void volgende() {
-        if (this.volgendeCijfer(this.getCijfer()) == 0) {
-            if (this.volgendeLetter(this.getTweedeLetter()) == 'A') {
+        if (this.volgendeCijfer(this.getCijfer()) == START_CIJFER) {
+            if (this.volgendeLetter(this.getTweedeLetter()) == START_LETTER) {
                 this.setEersteLetter(this.volgendeLetter(this.getEersteLetter()));
             }
             this.setTweedeLetter(this.volgendeLetter(this.getTweedeLetter()));
         }
         this.setCijfer(this.volgendeCijfer(this.getCijfer()));
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%c%c%d", this.getEersteLetter(), this.getTweedeLetter(), this.getCijfer());
     }
 
     public void setEersteLetter(char eersteLetter) {
